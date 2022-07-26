196b43e53d5e33e552ffea0efe0f1704fdfd865a
KTx735 <mygame1980@hotmail.com>
2022-07-26 12:23:08 -0600

    Update 3.0
    - Level 4, Level 5 & Level 6 Added
    - All 6 levels have their own background music.
    
    Needed Fixes:
    - Right boundary needs to be fixed to look more pleasing
    - Lower difficulty on Level 5

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    float movementFactor;
+    [SerializeField] float period = 2f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (period <= Mathf.Epsilon) { return; }
+        float cycles = Time.time / period;  // continually growing over time
+        
+        const float tau = Mathf.PI * 2;  // constant value of 6.283
+        float rawSinWave = Mathf.Sin(cycles * tau);  // going from -1 to 1
+
+        movementFactor = (rawSinWave + 1f) / 2f;   // recalculated to go from 0 to 1 so its cleaner
+        
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+}
